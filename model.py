47f6caf2c7dab7d715d039d09355b43d8e1451cf
varad-comrad <fabricio_asfora_002@hotmail.com>
2024-03-01 20:21:25 -0300

    started developing HERTA documentation

--- model.py
+++ model.py
@@ -1,4 +1,32 @@
 import argparse
+import torch
+import accelerate
+import lightning
+import torch.nn as nn
+from torch.utils.data import DataLoader, Dataset
+import pytorch_lightning as pl
+import torch.optim as optim
+import pandas as pd
+import json
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, max_len=5000, device=torch.device('cuda')):
+        super(PositionalEncoding, self).__init__()
+        pe = torch.zeros(max_len, d_model).to(device, dtype=torch.float64)
+        position = torch.arange(0, max_len, dtype=torch.float64).unsqueeze(1).to(device, dtype=torch.float64)
+        div_term = torch.exp(torch.arange(0, d_model, 2).to(device, dtype=torch.float64) * (-torch.log(torch.tensor(10000.0)) / d_model)).to(device, dtype=torch.float64)
+        pe[:, 0::2] = torch.sin(position * div_term).to(device, dtype=torch.float64)
+        pe[:, 1::2] = torch.cos(position * div_term).to(device, dtype=torch.float64)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return x + self.pe[:x.size(0), :]
+    
+
+
+
 
 def get_user_input() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description='Download stock data from Alpha Vantage.')
